8fdff7bfc2bafc55358ab156901a0f66a8d06766
이창환 <lch@team-mono.com>
2024-11-01 18:29:51 +0900

    feat: 데일리 스크럼 봇

--- apis/slack.py
+++ apis/slack.py
@@ -21,11 +21,24 @@ HEADERS: Dict[str, str] = {
 def get_slack_users() -> List[Dict[str, Any]]:
     """Slack 사용자 목록을 가져옵니다."""
     url: str = 'https://slack.com/api/users.list'
-    response = requests.get(url, headers={'Authorization': f'Bearer {SLACK_BOT_TOKEN}'})
+    response = requests.get(url, headers=HEADERS)
     response.raise_for_status()
     data: Dict[str, Any] = response.json()
     if not data.get('ok'):
         raise Exception(f"Error fetching Slack users: {data.get('error')}")
+    return data.get('members', [])
+
+def get_slack_user_ids_in_channel(channel_id: str) -> List[str]:
+    """Slack 채널의 사용자 ID 목록을 가져옵니다."""
+    url: str = 'https://slack.com/api/conversations.members'
+    payload: Dict[str, str] = {
+        'channel': channel_id
+    }
+    response = requests.get(url, params=payload, headers=HEADERS)
+    response.raise_for_status()
+    data: Dict[str, Any] = response.json()
+    if not data.get('ok'):
+        raise Exception(f"Error fetching Slack channel members: {data.get('error')}")
     return data.get('members', [])
 
 def send_slack_message(channel_id: str, message: str) -> Dict[str, Any]:
@@ -38,3 +51,58 @@ def send_slack_message(channel_id: str, message: str) -> Dict[str, Any]:
     response = requests.post(url, json=payload, headers=HEADERS)
     response.raise_for_status()
     return response.json()
+
+def get_user_info(user_id) -> Dict[str, Any]:
+    """Slack 사용자 정보를 가져옵니다."""
+    url: str = 'https://slack.com/api/users.info'
+    payload: Dict[str, str] = {
+        'user': user_id
+    }
+    response = requests.get(url, params=payload, headers=HEADERS)
+    response.raise_for_status()
+    data: Dict[str, Any] = response.json()
+    if not data.get('ok'):
+        raise Exception(f"Error fetching Slack user info: {data.get('error')}")
+    return data.get('user', {})
+
+def create_canvas():
+    url = "https://slack.com/api/canvases.create"
+    data = {
+        "title": "신나는 데일리 스크럼!",
+        "document_content": {"type": "markdown", "markdown": "> standalone canvas!"}
+    }
+    response = requests.post(url, headers=HEADERS, json=data)
+    response_data = response.json()
+    if response_data.get("ok"):
+        return response_data["canvas_id"]
+    else:
+        raise Exception("캔버스 생성 실패: " + response_data.get("error", "Unknown error"))
+
+def edit_canvas(canvas_id, changes):
+    url = "https://slack.com/api/canvases.edit"
+    data = {
+        "canvas_id": canvas_id,
+        "changes": changes
+    }
+    response = requests.post(url, headers=HEADERS, json=data)
+    response_data = response.json()
+    if not response_data.get("ok"):
+        print(response_data)
+        raise Exception("캔버스 편집 실패: " + response_data.get("error", "Unknown error"))
+
+def lookup_sections(canvas_id):
+    url = "https://slack.com/api/canvases.sections.lookup"
+    data = {
+        "canvas_id": canvas_id,
+        "criteria": {
+            "contains_text": " "
+        }
+    }
+    response = requests.post(url, headers=HEADERS, json=data)
+    response_data = response.json()
+
+    if response_data.get("ok"):
+        return response_data["sections"]
+    else :
+        print(response_data)
+        raise Exception("캔버스 섹션 조회 실패: " + response_data.get("error", "Unknown error"))
---
+++ daily_scrum.py
@@ -0,0 +1,66 @@
+# daily_scrum.py
+
+import random
+from datetime import datetime
+from dotenv import load_dotenv
+
+from apis.slack import get_slack_user_ids_in_channel, send_slack_message, get_user_info, lookup_sections, edit_canvas
+
+# 환경 변수 로드
+load_dotenv()
+
+# Slack 채널 ID
+SLACK_CHANNEL_ID = 'C02JX95U7AP'  # 또는 환경 변수로 설정
+
+# 이모지 목록
+emojis = ["😀", "😃", "😄", "😁", "😆", "😅", "😂", "🤣", "😊", "😇", ":party-blob:", ":sad_cat_thumbs_up:"]
+
+
+def daily_scrum():
+    # Slack 사용자 목록 가져오기
+    user_ids = get_slack_user_ids_in_channel(SLACK_CHANNEL_ID)
+    # 봇 사용자 제외
+    user_id_to_user_info = {user_id: get_user_info(
+        user_id) for user_id in user_ids}
+    user_ids = [
+        user_id for user_id in user_ids if not user_id_to_user_info[user_id].get('is_bot', False)
+    ]
+    # 사용자 순서 랜덤 셔플
+    random.shuffle(user_ids)
+    # 사용자 멘션 생성
+    mentions = ' '.join([f'<@{user_id}>' for user_id in user_ids])
+    # 메시지 생성
+    message = f"🚀 *데일리 스크럼 시간입니다!* \n{mentions}\n\n오늘 스크럼에 참여 가능하신가요?\n참여 가능하면 보드의 체크박스를 눌러주세요! ✅"
+    # 메시지 전송
+    send_slack_message(SLACK_CHANNEL_ID, message)
+
+    canvas_id = 'F07UUHABV1P'
+
+    sections = lookup_sections(canvas_id)
+
+    # 캔버스 내용 지우기
+    for section in sections:
+        edit_canvas(
+            canvas_id, [{'operation': 'delete', 'section_id': section['id']}])
+
+    # 캔버스 내용 생성
+    today = datetime.now().strftime("%Y년 %m월 %d일")
+    content = f"{today} 출석부\n"
+    for user_id in user_ids:
+        user_info = user_id_to_user_info[user_id]
+        user_name = user_info.get('real_name', 'Unknown User')
+        emoji = random.choice(emojis)
+        content += f"- [ ] {user_name} {emoji}\n"
+
+    # 캔버스 편집
+    edit_canvas(canvas_id, [{
+        'operation': 'insert_at_end',
+        "document_content": {
+            "type": "markdown",
+            "markdown": content
+        }
+    }])
+
+
+if __name__ == "__main__":
+    daily_scrum()
