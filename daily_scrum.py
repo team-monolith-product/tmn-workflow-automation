4eb87318ec930ff934dcb294f14f5f2e732c6554
이창환 <lch@team-mono.com>
2025-04-02 22:07:58 +0900

    feat: 팀 구조에 맞게 셔플

--- daily_scrum.py
+++ daily_scrum.py
@@ -1,7 +1,9 @@
 # daily_scrum.py
+import argparse
 import os
 import random
 from datetime import datetime
+from typing import Dict, List
 from dotenv import load_dotenv
 
 import requests
@@ -31,6 +33,17 @@ emojis = ["😀", "😃", "😄", "😁", "😆", "😅", "😂", "🤣", "😊"
 
 
 def daily_scrum():
+    """
+    --dry-run
+      옵션이 주어지는 경우 실제 메시지를 전송하지 않고,
+      대신 콘솔에 출력합니다.
+    """
+    # 명령행 인자 파싱
+    parser = argparse.ArgumentParser(description="근무 시간 알림 스크립트")
+    parser.add_argument('--dry-run', action='store_true',
+                        help='메시지를 Slack에 전송하지 않고 콘솔에 출력합니다.')
+    args = parser.parse_args()
+
     slack_client = WebClient(token=os.environ.get("SLACK_BOT_TOKEN"))
 
     # 1) 원티드스페이스에서 오늘자 WorkEvent(휴가/외근)를 받아옵니다.
@@ -46,30 +59,17 @@ def daily_scrum():
     # Slack 사용자 목록 가져오기
     user_ids = slack_client.conversations_members(
         channel=SLACK_CHANNEL_ID)["members"]
+    
     # 봇 사용자 제외
-
     user_id_to_user_info = {
         user_id: slack_client.users_info(user=user_id)['user'] for user_id in user_ids
     }
     user_ids = [
         user_id for user_id in user_ids if not user_id_to_user_info[user_id].get('is_bot', False)
     ]
-    # 사용자 순서 랜덤 셔플
-    random.shuffle(user_ids)
 
-    sections = slack_client.canvases_sections_lookup(
-        canvas_id=SLACK_CANVAS_ID,
-        criteria={
-            "contains_text": " "
-        }
-    )["sections"]
-
-    # 캔버스 내용 지우기
-    for section in sections:
-        slack_client.canvases_edit(
-            canvas_id=SLACK_CANVAS_ID,
-            changes=[{'operation': 'delete', 'section_id': section['id']}]
-        )
+    # 최적의 스크럼 효율을 위해 참여자의 순서를 조작합니다.
+    user_ids = shuffle(slack_client, user_ids)
 
     # 캔버스 내용 생성
     today = datetime.now().strftime("%Y년 %m월 %d일")
@@ -83,24 +83,42 @@ def daily_scrum():
         user_email = user_profile.get('email', "")
 
         # ex) '연차(오후)'
-        event_reason = email_to_event.get(user_email, "")  
+        event_reason = email_to_event.get(user_email, "")
 
         if event_reason:
             content += f"- [ ] {user_name} {emoji} - {event_reason}\n"
         else:
             content += f"- [ ] {user_name} {emoji}\n"
 
-    # 캔버스 편집
-    slack_client.canvases_edit(
-        canvas_id=SLACK_CANVAS_ID,
-        changes=[{
-        'operation': 'insert_at_end',
-        "document_content": {
-            "type": "markdown",
-            "markdown": content
-        }
-    }]
-    )
+    if args.dry_run:
+        # 실제 캔버스를 수정하지 않고 콘솔에 출력합니다.
+        print(f"캔버스:\n{content}")
+    else:
+        # 캔버스 편집
+        sections = slack_client.canvases_sections_lookup(
+            canvas_id=SLACK_CANVAS_ID,
+            criteria={
+                "contains_text": " "
+            }
+        )["sections"]
+
+        # 캔버스 내용 지우기
+        for section in sections:
+            slack_client.canvases_edit(
+                canvas_id=SLACK_CANVAS_ID,
+                changes=[{'operation': 'delete', 'section_id': section['id']}]
+            )
+
+        slack_client.canvases_edit(
+            canvas_id=SLACK_CANVAS_ID,
+            changes=[{
+                'operation': 'insert_at_end',
+                "document_content": {
+                    "type": "markdown",
+                    "markdown": content
+                }
+            }]
+        )
 
 
 def get_wantedspace_workevent():
@@ -146,6 +164,90 @@ def get_wantedspace_workevent():
     }
     response = requests.get(url, params=query, headers=headers, timeout=10)
     return response.json()
+
+
+def shuffle(
+    slack_client: WebClient,
+    user_ids: List[str],
+) -> List[str]:
+    """
+    최적의 스크럼 효율을 위해 참여자의 순서를 조작합니다.
+    - 기본적으로 무작위로 배치하여 매일 앞 사람의 발표에 집중하게 합니다.
+    - 같은 팀 구성원들은 서로 가까이 배치하여 듣는 사람의 이해를 돕습니다.
+
+    Args:
+        user_ids (List[str]): 사용자 ID 목록
+
+    Returns:
+        List[str]: 무작위로 섞인 사용자 ID 목록
+    """
+
+    team_id_to_user_ids = get_team_id_to_user_ids(slack_client, user_ids)
+
+    # 팀별로 사용자 ID를 무작위로 섞습니다.
+    for team_id, uids in team_id_to_user_ids.items():
+        team_id_to_user_ids[team_id] = random.sample(uids, len(uids))
+
+    # 팀을 무작위로 섞습니다.
+    team_ids = list(team_id_to_user_ids.keys())
+    random.shuffle(team_ids)
+
+    return [
+        user_id for team in team_ids for user_id in team_id_to_user_ids[team]
+    ]
+
+
+def get_team_id_to_user_ids(
+    slack_client: WebClient,
+    user_ids: List[str],
+) -> Dict[str | None, List[str]]:
+    """
+    Slack SDK를 사용하여 사용자 ID와 팀(사용자 그룹)을 매핑합니다.
+    한 사용자가 여러 사용자 그룹에 속한다면,
+    그 중 가장 작은 규모의 사용자 그룹을 선택합니다.
+
+    Args:
+        user_ids (List[str]): 사용자 ID 목록
+
+    Returns:
+        Dict: 사용자 ID와 팀 매핑
+    """
+    team_id_to_user_ids = {}
+    usergroups_response = slack_client.usergroups_list()
+    for group in usergroups_response["usergroups"]:
+        team_id_to_user_ids[group["id"]] = slack_client.usergroups_users_list(usergroup=group["id"]).get("users", [])
+
+    # 사용자 ID와 팀 매핑 (최소 규모 팀 )
+    user_id_to_team_ids = {}
+    for team_id, user_ids in team_id_to_user_ids.items():
+        for user_id in user_ids:
+            if user_id not in user_id_to_team_ids:
+                user_id_to_team_ids[user_id] = []
+            user_id_to_team_ids[user_id].append(team_id)
+
+    # 최소 규모 팀을 선택
+    user_id_to_smallest_team_id = {}
+    for user_id, team_ids in user_id_to_team_ids.items():
+        if len(team_ids) > 1:
+            min_team_id = min(team_ids, key=lambda x: len(team_id_to_user_ids[x]))
+            user_id_to_smallest_team_id[user_id] = min_team_id
+        else:
+            user_id_to_smallest_team_id[user_id] = team_ids[0]
+
+
+    # 팀 ID가 없는 사용자 ID는 None으로 설정
+    for user_id in user_ids:
+        if user_id not in user_id_to_smallest_team_id:
+            user_id_to_smallest_team_id[user_id] = None
+
+
+    smallest_team_id_to_user_ids = {}
+    for user_id, team_id in user_id_to_smallest_team_id.items():
+        if team_id not in smallest_team_id_to_user_ids:
+            smallest_team_id_to_user_ids[team_id] = []
+        smallest_team_id_to_user_ids[team_id].append(user_id)
+
+    return smallest_team_id_to_user_ids
 
 if __name__ == "__main__":
     daily_scrum()
