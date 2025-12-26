48a8d35e5db922398779f0027cd01ed3983e7903
이창환 <lch@team-mono.com>
2025-12-26 23:43:42 +0900

    feat: 불필요한 환경변수 주입 제거

--- api/athena.py
+++ api/athena.py
@@ -17,8 +17,6 @@ def get_athena_client():
     """
     return boto3.client(
         "athena",
-        aws_access_key_id=os.environ.get("AWS_ACCESS_KEY_ID"),
-        aws_secret_access_key=os.environ.get("AWS_SECRET_ACCESS_KEY"),
         region_name=os.environ.get("AWS_REGION", "ap-northeast-2"),
     )
 
